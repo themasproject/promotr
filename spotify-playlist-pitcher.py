b1c676229edc6b4c361d8469e1bf224e7fe4a6e3
themasproject <michael.sawers52@gmail.com>
2025-12-31 00:40:01 -0500

    figuring out some stuff here

--- spotify-playlist-pitcher.py
+++ spotify-playlist-pitcher.py
@@ -60,7 +60,13 @@ try:
 except urllib.error.HTTPError as e:
     print(f"Error: {e}")
 
-print(result_dict['playlists']['items'])
+# print(result_dict['playlists']['items'][0].keys())
+print(result_dict['playlists']['items'][0])
 
 # only save those with over 100,000 saves
 matches = []
+
+for i in result_dict['playlists']['items']:
+    # here, I need to create a function for the api call so I can stop repeating so much code. then GET playlist
+    # need to filter for values that represent LEGTITIMATE playlists. 
+    # time on playlist, whether followers of the playulist are bgeing banned or not, etc etc
